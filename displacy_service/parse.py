3391a8cf0e446dc7a3899dc529350b615dae9abb
olivier <olivier.terrier@kairntech.com>
2019-04-02 10:17:03 +0200

    Added feature/endpoint allowing a genuine POS tagger response

--- displacy_service/parse.py
+++ displacy_service/parse.py
@@ -63,6 +63,103 @@ class Entities(object):
             } for ent in self.doc.ents
         ]
 
+class Tokens(object):
+    def __init__(self, nlp, text, include_sentences, attr_filter):
+        self.doc = nlp(text)
+        self.filter = attr_filter
+        self.inc_sents = include_sentences
+
+    def to_json(self):
+        if self.inc_sents:
+            return [ self.sent_to_dict(sent) for sent in self.doc.sents]
+        else:
+            return [ self.token_to_dict(tok) for tok in self.doc ]
+
+    def sent_to_dict(self, sent):
+        all = len(self.filter) == 0
+        attrs = {
+            'text': sent.text,
+            'start': sent.start_char,
+            'end': sent.end_char,
+            "tokens" : [ self.token_to_dict(tok) for tok in sent ]
+        }
+        # if all or 'vector' in self.filter:
+        #     attrs['vector'] = sent.vector
+        return attrs
+
+    def token_to_dict(self, tok):
+        all = len(self.filter) == 0
+        attrs = {
+            'start': tok.idx,
+            'end': tok.idx + len(tok),
+        }
+        if all or 'text' in self.filter:
+            attrs['text'] = tok.text
+        if all or 'orth' in self.filter:
+            attrs['orth'] = tok.orth_
+        if all or 'lemma' in self.filter:
+            attrs['lemma'] = tok.lemma_
+        if all or 'pos' in self.filter:
+            attrs['pos'] = tok.pos_
+        if all or 'tag' in self.filter:
+            attrs['tag'] = tok.tag_
+        if all or 'dep' in self.filter:
+            attrs['dep'] = tok.dep_
+        # if all or 'vector' in self.filter:
+        #     attrs['vector'] = tok.vector.tolist()
+        if all or 'ent_type' in self.filter:
+            attrs['ent_type'] = tok.ent_type_
+        if all or 'ent_iob_' in self.filter:
+            attrs['ent_iob'] = tok.ent_iob_
+        if all or 'norm' in self.filter:
+            attrs['norm'] = tok.norm_
+        if all or 'lower' in self.filter:
+            attrs['lower'] = tok.lower_
+        if all or 'shape' in self.filter:
+            attrs['shape'] = tok.shape_
+        if all or 'prefix' in self.filter:
+            attrs['prefix'] = tok.prefix_
+        if all or 'suffix' in self.filter:
+            attrs['suffix'] = tok.suffix_
+        if all or 'is_alpha' in self.filter:
+            attrs['is_alpha'] = tok.is_alpha
+        if all or 'is_ascii' in self.filter:
+            attrs['is_ascii'] = tok.is_ascii
+        if all or 'is_digit' in self.filter:
+            attrs['is_digit'] = tok.is_digit
+        if all or 'is_lower' in self.filter:
+            attrs['is_lower'] = tok.is_lower
+        if all or 'is_upper' in self.filter:
+            attrs['is_upper'] = tok.is_upper
+        if all or 'is_title' in self.filter:
+            attrs['is_title'] = tok.is_title
+        if all or 'is_punct' in self.filter:
+            attrs['is_punct'] = tok.is_punct
+        if all or 'is_left_punct' in self.filter:
+            attrs['is_left_punct'] = tok.is_left_punct
+        if all or 'is_right_punct' in self.filter:
+            attrs['is_right_punct'] = tok.is_right_punct
+        if all or 'is_space' in self.filter:
+            attrs['is_space'] = tok.is_space
+        if all or 'is_bracket' in self.filter:
+            attrs['is_bracket'] = tok.is_bracket
+        if all or 'is_quote' in self.filter:
+            attrs['is_quote'] = tok.is_quote
+        if all or 'is_currency' in self.filter:
+            attrs['is_currency'] = tok.is_currency
+        if all or 'like_url' in self.filter:
+            attrs['like_url'] = tok.like_url
+        if all or 'like_num' in self.filter:
+            attrs['like_num'] = tok.like_num
+        if all or 'like_email' in self.filter:
+            attrs['like_email'] = tok.like_email
+        if all or 'is_oov' in self.filter:
+            attrs['is_oov'] = tok.is_oov
+        if all or 'is_stop' in self.filter:
+            attrs['is_stop'] = tok.is_stop
+        if all or 'cluster' in self.filter:
+            attrs['cluster'] = tok.cluster
+        return attrs
 
 class Sentences(object):
     def __init__(self, nlp, text):
--- displacy_service_tests/test_server.py
+++ displacy_service_tests/test_server.py
@@ -5,41 +5,107 @@ from displacy_service.server import APP
 
 
 class TestAPI(falcon.testing.TestCase):
-    def __init__(self):
-        self.api = APP
+    def setUp(self):
+        super(TestAPI, self).setUp()
+        self.app = APP
 
+    def test_deps(self):
+        result = self.simulate_post(
+            path='/dep',
+            body='''{"text": "This is a test.", "model": "en",
+                 "collapse_punctuation": false,
+                 "collapse_phrases": false}'''
+        )
+        result = json.loads(result.text)
+        words = [w['text'] for w in result['words']]
+        assert words == ["This", "is", "a", "test", "."]
 
-def test_deps():
-    test_api = TestAPI()
-    result = test_api.simulate_post(
-        path='/dep',
-        body='''{"text": "This is a test.", "model": "en",
-             "collapse_punctuation": false,
-             "collapse_phrases": false}'''
-    )
-    result = json.loads(result.text)
-    words = [w['text'] for w in result['words']]
-    assert words == ["This", "is", "a", "test", "."]
+    def test_ents(self):
+        result = self.simulate_post(
+            path='/ent',
+            body='''{"text": "What a great company Google is.",
+                    "model": "en"}''')
+        ents = json.loads(result.text)
+        assert ents == [
+            {"start": 21, "end": 27, "type": "ORG", "text": "Google"}]
 
+    def test_tag_full(self):
+        toks = self.simulate_post(
+            path='/tag',
+            json={
+                "text": "Foo",
+                "model": "en",
+            }).json
+        assert toks[0] == {'start': 0, 'end': 3, 'text': 'Foo', 'orth' : 'Foo', 'lemma': 'foo', 'pos': 'PROPN', 'tag': 'NNP',
+                           'dep': 'ROOT', 'ent_type': '', 'ent_iob': 'O', 'norm': 'foo',
+                           'lower': 'foo', 'shape': 'Xxx', 'prefix': 'F', 'suffix': 'Foo', 'is_alpha': True,
+                           'is_ascii': True, 'is_digit': False, 'is_lower': False, 'is_upper': False, 'is_title': True,
+                           'is_punct': False, 'is_left_punct': False, 'is_right_punct': False, 'is_space': False,
+                           'is_bracket': False, 'is_quote': False, 'is_currency': False, 'like_url': False,
+                           'like_num': False, 'like_email': False, 'is_oov': True, 'is_stop': False, 'cluster': 0}
 
-def test_ents():
-    test_api = TestAPI()
-    result = test_api.simulate_post(
-        path='/ent',
-        body='''{"text": "What a great company Google is.",
-                "model": "en"}''')
-    ents = json.loads(result.text)
-    assert ents == [
-        {"start": 21, "end": 27, "type": "ORG", "text": "Google"}]
+    def test_tag_with_filter(self):
+        toks = self.simulate_post(
+            path='/tag',
+            json={
+                "text": "Fed raises interest rates 0.5 percent.",
+                "model": "en",
+                "attr_filter": ["text", "start", "end", "lemma", "pos"]
+            }).json
 
+        assert toks == [{'start': 0, 'end': 3, 'text': 'Fed', 'lemma': 'fed', 'pos': 'PROPN'},
+                        {'start': 4, 'end': 10, 'text': 'raises', 'lemma': 'raise', 'pos': 'VERB'},
+                        {'start': 11, 'end': 19, 'text': 'interest', 'lemma': 'interest', 'pos': 'NOUN'},
+                        {'start': 20, 'end': 25, 'text': 'rates', 'lemma': 'rate', 'pos': 'NOUN'},
+                        {'start': 26, 'end': 29, 'text': '0.5', 'lemma': '0.5', 'pos': 'NUM'},
+                        {'start': 30, 'end': 37, 'text': 'percent', 'lemma': 'percent', 'pos': 'NOUN'},
+                        {'start': 37, 'end': 38, 'text': '.', 'lemma': '.', 'pos': 'PUNCT'}]
 
-def test_sents():
-    test_api = TestAPI()
-    sentences = test_api.simulate_post(
-        path='/sent',
-        body='''{"text": "This a test that should split into sentences!
-        This is the second. Is this the third?", "model": "en"}'''
-    )
+    def test_tag_with_sents(self):
+        sents = self.simulate_post(
+            path='/tag',
+            json={
+                "text": "This a test that should split into sentences! This is the second.",
+                "model": "en",
+                "include_sentences": True,
+                "attr_filter": ["text", "start", "end", "lemma", "pos"]
+            }).json
+        assert sents == [
+            {'text': 'This a test that should split into sentences!',
+             'start': 0,
+             'end': 45,
+             'tokens': [
+                 {'text': 'This', 'start': 0, 'end': 4, 'lemma': 'this', 'pos': 'DET'},
+                 {'text': 'a', 'start': 5, 'end': 6, 'lemma': 'a', 'pos': 'DET'},
+                 {'text': 'test', 'start': 7, 'end': 11, 'lemma': 'test', 'pos': 'NOUN'},
+                 {'text': 'that', 'start': 12, 'end': 16, 'lemma': 'that', 'pos': 'ADJ'},
+                 {'text': 'should', 'start': 17, 'end': 23, 'lemma': 'should', 'pos': 'VERB'},
+                 {'text': 'split', 'start': 24, 'end': 29, 'lemma': 'split', 'pos': 'VERB'},
+                 {'text': 'into', 'start': 30, 'end': 34, 'lemma': 'into', 'pos': 'ADP'},
+                 {'text': 'sentences', 'start': 35, 'end': 44, 'lemma': 'sentence', 'pos': 'NOUN'},
+                 {'text': '!', 'start': 44, 'end': 45, 'lemma': '!', 'pos': 'PUNCT'}
+             ]},
+            {
+                'text': 'This is the second.',
+                'start': 46,
+                'end': 65,
+                'tokens': [
+                    {'text': 'This', 'start': 46, 'end': 50, 'lemma': 'this', 'pos': 'DET'},
+                    {'text': 'is', 'start': 51, 'end': 53, 'lemma': 'be', 'pos': 'VERB'},
+                    {'text': 'the', 'start': 54, 'end': 57, 'lemma': 'the', 'pos': 'DET'},
+                    {'text': 'second', 'start': 58, 'end': 64, 'lemma': 'second', 'pos': 'ADJ'},
+                    {'text': '.', 'start': 64, 'end': 65, 'lemma': '.', 'pos': 'PUNCT'}
+                ]}
+        ]
 
-    assert sentences == ['This a test that should split into sentences!',
-                         'This is the second.', 'Is this the third?']
+    def test_sents(self):
+        sentences = self.simulate_post(
+            path='/sents',
+            json={
+                "text": """This a test that should split into sentences!
+                This is the second. Is this the third?""",
+                "model": "en"
+            }
+        )
+        assert sentences.json == ['This a test that should split into sentences!',
+                             'This is the second.', 'Is this the third?']
