692d3b9c5725c7ae3ba211ebbd295ba87a162499
Johannes Gontrum <johannes@knowing.se>
2022-08-08 17:13:36 +0200

    Cleanup

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 
 setup(
     name="displacy_service",
-    version="0.1",
-    description="REST microservice for Explosion AI's interactive demos "
-    + "and visualisers.",
+    version="0.3",
+    description="REST microservice for spaCy.",
     author="explosion.ai & Johannes Gontrum",
     author_email="gontrum@me.com",
     include_package_data=True,
